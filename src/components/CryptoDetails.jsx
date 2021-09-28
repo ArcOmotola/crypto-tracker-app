a3b02f2c32f6f7bbc94cb1f80b81dbc71c3ee69f
ArcOmotola <ogunsola40@gmail.com>
2021-09-28 13:20:45 +0100

    mobile responsive nav bar feature

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -21,6 +21,7 @@ export default function CryptoDetails() {
     if(isFetching) return "Loading...";
 
     console.log("single crypto>>>", data);
+    console.log("price history>>>", coinHistory);
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 

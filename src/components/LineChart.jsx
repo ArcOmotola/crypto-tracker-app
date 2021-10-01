6a0fa164541f2a25555dc4ba7a0a9fb7bcfeb38b
ArcOmotola <ogunsola40@gmail.com>
2021-10-01 16:45:03 +0100

    fixed css linegraph breaking points

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -27,6 +27,7 @@ export default function LineChart({ coinHistory, currentPrice, coinName}) {
     }
 
     const options = {
+        maintainAspectRatio: false,
         scales: {
             yAxes: [
                 {
@@ -40,14 +41,16 @@ export default function LineChart({ coinHistory, currentPrice, coinName}) {
 
     return (
         <>
-            <Row className>
+            <Row className="chart-header">
                 <Title level={2} className="chart-title">{coinName} Price Chart</Title>
                 <Col className="price-container">
                     <Title level={5} className="price-change">{ coinHistory?.data?.change}%</Title>
                     <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
                 </Col>
             </Row>
-            <Line data={data} options={options}/>
+            <div className="chart-wrapper" >
+                    <Line data={data} options={options}/>
+            </div>
         </>
     )
 }
